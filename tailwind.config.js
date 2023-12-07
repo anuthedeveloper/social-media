6861b23fd7649153892a1f6a3eba8ea2a6fbcd23
Okanlawon Aanuoluwapo <tunsabaze01@gmail.com>
2023-12-07 08:20:42 +0000

    To fix error on dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        socialBg:'F5F7FB',
+        socialBg:'#F5F7FB',
         socialBlue: '#218DFA',
       },
     },
